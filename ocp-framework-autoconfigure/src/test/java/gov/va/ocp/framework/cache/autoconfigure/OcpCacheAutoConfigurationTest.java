c9d0667adab83927d0ee3ffb0ca32ed82d4130e7
varun-akula <vakula@meetveracity.com>
2019-03-11 14:53:23 -0400

    CMAPI2-86 Achieve coverage targets

--- ocp-framework-autoconfigure/src/test/java/gov/va/ocp/framework/cache/autoconfigure/OcpCacheAutoConfigurationTest.java
+++ ocp-framework-autoconfigure/src/test/java/gov/va/ocp/framework/cache/autoconfigure/OcpCacheAutoConfigurationTest.java
@@ -1,8 +1,13 @@
 package gov.va.ocp.framework.cache.autoconfigure;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import java.lang.reflect.Method;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -21,16 +26,18 @@ import gov.va.ocp.framework.cache.autoconfigure.OcpCacheAutoConfiguration;
  * Created by vgadda on 8/11/17.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-public class ReferenceCacheAutoConfigurationTests {
+public class OcpCacheAutoConfigurationTest {
+
+	private static final String SPRING_CACHE_TYPE_PROPERTY_AND_VALUE = "spring.cache.type=redis";
 
 	private AnnotationConfigApplicationContext context;
-
+	
 	@Mock
 	CacheManager cacheManager;
 
 	@Mock
 	Cache mockCache;
-
+	
 	@After
 	public void close() {
 		if (this.context != null) {
@@ -41,18 +48,41 @@ public class ReferenceCacheAutoConfigurationTests {
 	@Test
 	public void testReferenceCacheConfiguration() throws Exception {
 		context = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of("spring.cache.type=redis").applyTo(context);
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
 		context.register(RedisAutoConfiguration.class, OcpCacheAutoConfiguration.class);
 		context.refresh();
 		assertNotNull(context);
 		CacheManager cacheManager = context.getBean(CacheManager.class);
 		assertNotNull(cacheManager);
 	}
+	
+	@Test
+	public void testReferenceCacheConfigurations() throws Exception {
+		context = new AnnotationConfigApplicationContext();
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
+		TestPropertyValues.of("ocp.cache.defaultExpires=86401").applyTo(context);
+		context.register(RedisAutoConfiguration.class, OcpCacheProperties.class);
+		context.refresh();
+		OcpCacheProperties ocpCacheProperties = context.getBean(OcpCacheProperties.class);
+		OcpCacheProperties.RedisExpires redisExpires = new gov.va.ocp.framework.cache.autoconfigure.OcpCacheProperties.RedisExpires();
+		redisExpires.setName("testName");
+		redisExpires.setTtl(1500L);
+		List<OcpCacheProperties.RedisExpires> expiresList = new LinkedList<OcpCacheProperties.RedisExpires>();
+		expiresList.add(redisExpires);
+		ocpCacheProperties.setExpires(expiresList);
+		context.register(OcpCacheAutoConfiguration.class);
+		OcpCacheAutoConfiguration ocpCacheAutoConfiguration = context.getBean(OcpCacheAutoConfiguration.class);
+		
+		Map<String, org.springframework.data.redis.cache.RedisCacheConfiguration> cacheConfigs = ocpCacheAutoConfiguration.redisCacheConfigurations();
+		
+		assertNotNull(cacheConfigs);
+		assertEquals(cacheConfigs.get("testName").getTtl().getSeconds(), 1500L);
+	}
 
 	@Test
 	public void testReferenceCacheConfigurationKeyGenerator() throws Exception {
 		context = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of("spring.cache.type=redis").applyTo(context);
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
 		context.register(RedisAutoConfiguration.class, OcpCacheAutoConfiguration.class);
 		context.refresh();
 		assertNotNull(context);
@@ -64,7 +94,7 @@ public class ReferenceCacheAutoConfigurationTests {
 	@Test
 	public void testCacheGetError() throws Exception {
 		context = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of("spring.cache.type=redis").applyTo(context);
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
 		context.register(RedisAutoConfiguration.class, OcpCacheAutoConfiguration.class);
 		context.refresh();
 		assertNotNull(context);
@@ -76,7 +106,7 @@ public class ReferenceCacheAutoConfigurationTests {
 	@Test
 	public void testCachePutError() throws Exception {
 		context = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of("spring.cache.type=redis").applyTo(context);
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
 		context.register(RedisAutoConfiguration.class, OcpCacheAutoConfiguration.class);
 		context.refresh();
 		assertNotNull(context);
@@ -89,7 +119,7 @@ public class ReferenceCacheAutoConfigurationTests {
 	@Test
 	public void testCacheEvictError() throws Exception {
 		context = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of("spring.cache.type=redis").applyTo(context);
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
 		context.register(RedisAutoConfiguration.class, OcpCacheAutoConfiguration.class);
 		context.refresh();
 		assertNotNull(context);
@@ -101,7 +131,7 @@ public class ReferenceCacheAutoConfigurationTests {
 	@Test
 	public void testCacheClearError() throws Exception {
 		context = new AnnotationConfigApplicationContext();
-		TestPropertyValues.of("spring.cache.type=redis").applyTo(context);
+		TestPropertyValues.of(SPRING_CACHE_TYPE_PROPERTY_AND_VALUE).applyTo(context);
 		context.register(RedisAutoConfiguration.class, OcpCacheAutoConfiguration.class);
 		context.refresh();
 		assertNotNull(context);
---
+++ ocp-framework-autoconfigure/src/test/java/gov/va/ocp/framework/hystrix/autoconfigure/HystrixContextAwareConcurrencyStrategyTest.java
@@ -0,0 +1,49 @@
+package gov.va.ocp.framework.hystrix.autoconfigure;
+
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.Callable;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import gov.va.ocp.framework.hystrix.autoconfigure.HystrixCallableWrapper;
+import gov.va.ocp.framework.hystrix.autoconfigure.HystrixContextAwareConcurrencyStrategy;
+
+public class HystrixContextAwareConcurrencyStrategyTest {
+
+	@Test
+	public void testObjectCreation() {
+		assertNotNull(new HystrixContextAwareConcurrencyStrategy(new ArrayList<HystrixCallableWrapper>()));
+	}
+
+	@SuppressWarnings("unchecked")
+	@Test
+	public void shouldWrapCallable() {
+
+		@SuppressWarnings("rawtypes")
+		final Callable callable = Mockito.mock(Callable.class);
+
+		final HystrixContextAwareConcurrencyStrategy strategy = new HystrixContextAwareConcurrencyStrategy(
+				Collections.<HystrixCallableWrapper> singletonList(new SimpleHystrixCallableWrapper()));
+
+		assertNotEquals(callable, strategy.wrapCallable(callable));
+	}
+
+	private static class SimpleHystrixCallableWrapper implements HystrixCallableWrapper {
+
+		@Override
+		public <T> Callable<T> wrapCallable(final Callable<T> callable) {
+			return new Callable<T>() {
+				@Override
+				public T call() throws Exception {
+					return callable.call();
+				}
+			};
+		}
+	}
+
+}
