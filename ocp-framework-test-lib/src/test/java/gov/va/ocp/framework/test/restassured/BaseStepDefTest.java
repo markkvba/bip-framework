22881724de14a6952a14f9e3274283ae3428b17c
suchitharavi <sravi@ip-192-168-47-70.us-gov-west-1.compute.internal>
2019-03-19 09:55:22 -0400

    CMAPI2-108 Added wire mock dependency in pom.xml and upadted unit test scenario's

--- ocp-framework-test-lib/src/test/java/gov/va/ocp/framework/test/restassured/BaseStepDefTest.java
+++ ocp-framework-test-lib/src/test/java/gov/va/ocp/framework/test/restassured/BaseStepDefTest.java
@@ -1,9 +1,8 @@
 package gov.va.ocp.framework.test.restassured;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Matchers.anyObject;
 
 import java.io.File;
 import java.net.URL;
@@ -11,32 +10,71 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.io.FileUtils;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.util.ReflectionTestUtils;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
 
 import gov.va.ocp.framework.test.util.RESTUtil;
-import io.restassured.response.Response;
 
-@RunWith(MockitoJUnitRunner.class)
 public class BaseStepDefTest {
 
 	BaseStepDef subject = new BaseStepDef();
 
-	@Mock
-	Response response;
 
+	WireMockServer wireMockServer;
+	RESTUtil restUtil = new RESTUtil();
+	
 	@Before
-	public void setup() {
+    public void setup() {
+        wireMockServer = new WireMockServer(9999);
+        wireMockServer.start();
+        setupStub();
+        
 		Map<String, String> tblHeader = new HashMap<>();
 		tblHeader.put("Acept", "application/json");
 		tblHeader.put("Content-Type", "application/json");
 		subject.passHeaderInformation(tblHeader);	
 		subject.initREST();
+
+    }
+ 
+	@After
+    public void teardown () {
+        wireMockServer.stop();
+    }
+	
+	public void setupStub() {
+		addGetPersonStub();	
+		addPostPersonStub();
+		addDeletePersonStub();
+		addPostBearerStub();
+	}
+	
+	private void addGetPersonStub() {
+        wireMockServer.stubFor(get(urlEqualTo("/person"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBodyFile("json/get-person-response.json")));        
+    }
+	
+	private void addPostPersonStub() {
+        wireMockServer.stubFor(post(urlEqualTo("/person"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBodyFile("json/post-person-response.json")));        
+    }
+	private void addDeletePersonStub() {
+        wireMockServer.stubFor(delete(urlEqualTo("/person"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withBodyFile("json/delete-person-response.json")));        
+    }
+	
+	private void addPostBearerStub() {
+		wireMockServer.stubFor(post(urlEqualTo("/token"))
+				.willReturn(aResponse().withStatus(200).withBodyFile("bearer/post-bearer-response.txt")));
 	}
 	
 	@Test
@@ -46,33 +84,26 @@ public class BaseStepDefTest {
 	
 	@Test
 	public void test_invokeAPIUsingDelete_Success() {
-		RESTUtil restUtil = new RESTUtil();
-		RESTUtil spyRestUtil = Mockito.spy(restUtil);
-		Mockito.doNothing().when(spyRestUtil).doWithRetry(anyObject(), anyInt());
-		ReflectionTestUtils.setField(spyRestUtil, "response", response);
-		ReflectionTestUtils.setField(subject, "resUtil", spyRestUtil);
-		subject.invokeAPIUsingDelete("http://localhost/testUrl", false);
+		subject.invokeAPIUsingDelete("http://localhost:9999/person", false);
 	}
 	
 	@Test
 	public void test_invokeAPIUsingPost_Success() {
-		RESTUtil restUtil = new RESTUtil();
-		RESTUtil spyRestUtil = Mockito.spy(restUtil);
-		Mockito.doNothing().when(spyRestUtil).doWithRetry(anyObject(), anyInt());
-		ReflectionTestUtils.setField(spyRestUtil, "response", response);
-		ReflectionTestUtils.setField(subject, "resUtil", spyRestUtil);
-		subject.invokeAPIUsingPost("http://localhost/testUrl", false);
+		subject.invokeAPIUsingPost("http://localhost:9999/person", false);
+		String response = restUtil.postResponse("http://localhost:9999/person");
+		assertThat(true, equalTo(!response.isEmpty()));
 	}
 	
 	@Test
 	public void test_invokeAPIUsingGet_Success() {
-		RESTUtil restUtil = new RESTUtil();
-		RESTUtil spyRestUtil = Mockito.spy(restUtil);
-		Mockito.doNothing().when(spyRestUtil).doWithRetry(anyObject(), anyInt());
-		ReflectionTestUtils.setField(spyRestUtil, "response", response);
-		ReflectionTestUtils.setField(subject, "resUtil", spyRestUtil);
-		subject.invokeAPIUsingGet("http://localhost/testUrl", false);
+		subject.invokeAPIUsingGet("http://localhost:9999/person", false);
 	}
+	
+	@Test
+	public void test_invokeAPIUsingGet__WithBearerToken_Success() {
+		subject.invokeAPIUsingGet("http://localhost:9999/person", true);
+	}
+	
 	
 	@Test
 	public void test_setHeader_Success() throws Exception {
@@ -93,7 +124,7 @@ public class BaseStepDefTest {
 		final URL urlFilePath = BaseStepDefTest.class.getClassLoader().getResource("response/test.response");
 		final File strFilePath = new File(urlFilePath.toURI());
 		subject.strResponse = FileUtils.readFileToString(strFilePath, "ASCII");
-		assertThat(true, equalTo(subject.compareExpectedResponseWithActual("test.response")));
+		assertThat(true, equalTo(subject.compareExpectedResponseWithActualByRow("test.response")));
 	}
 		
 	@Test
