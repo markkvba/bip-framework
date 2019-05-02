429e0168e0c8ab3743a14ef2f58390ca131e1410
Abhijit Kulkarni <akulkarni@meetveracity.com>
2019-05-02 15:21:31 -0400

    Updated corrupted file and added IllegalArgumentException for load

--- bip-framework-test-lib/src/main/java/gov/va/bip/framework/test/util/PropertiesUtil.java
+++ bip-framework-test-lib/src/main/java/gov/va/bip/framework/test/util/PropertiesUtil.java
@@ -63,7 +63,7 @@ public class PropertiesUtil {
 			properties.load(input);
 			substitutePlaceholders(properties);
 
-		} catch (IOException | URISyntaxException e) {
+		} catch (IllegalArgumentException | IOException | URISyntaxException e) {
 			LOGGER.error(e.getMessage(), e);
 			throw new BipTestLibRuntimeException("Could not load the properties file with URL : " + fileUrl, (Throwable) e);
 		}
